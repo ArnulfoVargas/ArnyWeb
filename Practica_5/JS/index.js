a7821f3aaf7c3cb21cb45d864c9b5fae9bdb4be5
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-08-31 00:48:03 -0600

    commit

---
+++ Practica_5/JS/index.js
@@ -0,0 +1,31 @@
+const selection = document.getElementById("selection");
+const button = document.getElementById("button");
+const results = document.getElementById("results");
+
+let text = "";
+
+button.addEventListener("click", () => {
+    data()
+})
+
+const data = async() => {
+    try{
+        const endpoint = "http://www.boredapi.com/api/activity?type=" + selection.value;
+        const response = await fetch(endpoint);
+        const data = await response.json();
+
+        let {activity, participants} = data;
+
+        console.log(data);
+
+        text += "<p>" + activity + "</p>" +
+        "<p><strong>For: </strong> " + participants + " participants</p> <hr>";
+
+        console.log(text)
+
+        results.innerHTML = "<h1>Results</h1> <hr>" + text;
+    }
+    catch{
+        return null;
+    }
+};
