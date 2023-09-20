c8f075a2fab975ea1519cefe42587d8af62d49b7
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-09-20 01:44:51 -0600

    si

--- React/react_repoio/src/components/rightSquare/RightSquare.tsx
+++ React/react_repoio/src/components/rightSquare/RightSquare.tsx
@@ -2,7 +2,7 @@ function RightSquare() {
     return (
         <div className="my-5 flex justify-end px-10">
             <div className="bg-blue-800 border-black border-4 md:w-1/6 block p-5 font-bold text-white">
-                <p>Derecha</p>
+                <p className="mb-5">Derecha</p>
                 <p>Este cuadro esta posicionado a la derecha</p>
             </div>
         </div>
